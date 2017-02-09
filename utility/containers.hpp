929b74543264582f169f3def2ca055b3352be024
Krys <aslay.k@qq.com>
2017-02-09 16:52:49 +0800

    optional

--- utility/containers.hpp
+++ utility/containers.hpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <experimental/string_view>
 #include <boost/variant.hpp>
+#include <experimental/optional>
 
 using std::vector;
 using std::map;
@@ -11,5 +12,7 @@ using std::array;
 using std::experimental::string_view;
 using std::experimental::basic_string_view;
 using boost::variant;
+using std::experimental::optional;
+using std::experimental::nullopt;
 
 
